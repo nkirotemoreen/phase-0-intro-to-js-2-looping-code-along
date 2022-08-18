8c78114e82ef5c0b18307c9284152c8d3db02451
moreenisaac <nkirotemoreen70@gmail.com>
2022-08-18 12:57:21 +0300

    complete

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Code your solutions in this file
+const names = ["Guadalupe", "Ollie", "Aki"];
+
+function writeCards(names, surpise) {
+	const message = [];
+    //for loop
+	for (let i = 0; i < names.length; i++) {
+		const thankYou = `Thank you, ${names[i]}, for the wonderful ${surpise} gift!`;
+		message.push(thankYou);
+	}
+	return message;
+}
+
+ console.log(writeCards());
+ writeCards(names, "surprise");
+
+const number = 10;
+function countDown(number) {
+	let i = 10;
+	while (i >= 0) {
+		console.log(i--);
+	}
+	return number;
+
+}
+countDown(number); 
